a3aa4d7610205506a27b69273251b43fd4e5d758
august <august.lindgrenruby@signalwire.com>
2025-12-18 12:47:47 -0500

    hit-api: add comments

--- shell/hit-api.sh
+++ shell/hit-api.sh
@@ -11,8 +11,11 @@ fi
 # Set endpoint route
 ROUTE="api/calling/calls"
 
+# Send request
 curl -v -L -g "https://$SPACE.signalwire.com/$ROUTE" \
 	-u ${PROJECT_ID}:${TOKEN}
 	-H "Content-Type: application/json" \
 	-H "Accept: application/json" \
+
+	# Load request body from data.json 
 	--data @data.json
